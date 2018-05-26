b191f3cde7b849d29454c72b0085bfa1e924a8e5
svend <sven.depickere@student.howest.be>
2018-05-26 17:35:48 +0200

    Update routes with all possible error's

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 const knex = require('knex')(require('../Sql-data/sql-connection'));
 
-/* GET home page. */
+const index = require('./redirect/index');
+
+
+let insertValues = function(data, res){
+    knex('logindetails')
+        .insert([{Username: data.username, EmailAdress: data.email, Password: data.password}])
+        .then(
+            index.fillInLoginDetails(data,res)
+        );
+};
+
 router.get('/', function (req, res, next) {
-    res.render('index', {
-        user: {
-            username: null,
-            password: null
-        }
-    });
+    index.normalIndex(res);
 });
 
 router.post('/login', function (req, res, next) {
@@ -18,45 +23,48 @@ router.post('/login', function (req, res, next) {
         Username: data.username
     }).select('Password')
         .then(function (Password) {
-            switch (Password){
+            switch (Password) {
                 case undefined:
-                    renderHomeWithUserNameFilledIn(data);
+                    data.message = "Password or Username is wrong";
+                    index.renderHomeWithUserNameFilledIn(data,res);
                     break;
                 case Password[0].Password === data.password:
-                    login(data);
+                    index.login(data,res);
                     break;
                 default:
-                    renderHomeWithUserNameFilledIn(data);
+                    data.message = "Password or Username is wrong";
+                    index.renderHomeWithUserNameFilledIn(data,res);
                     break;
             }
         })
 });
 
-login = function(data){
-    res.render('home', {title: data.username})
-};
-
-renderHomeWithUserNameFilledIn = function(data) {
-    res.render('index', {
-        user: {
-            username: data.username,
-            password: null
-        }
-    })
-};
-
 router.post('/register', function (req, res, next) {
     let data = req.body;
-    knex('logindetails')
-        .insert([{Username: data.username, EmailAdress: data.email, Password: data.password}])
-        .then(
-            res.render('index', {
-                user: {
-                    username: data.username,
-                    password: data.password
+    if (data.password === data["confirm-password"]){
+        knex('logindetails')
+            .where({
+                Username: data.username,
+                EmailAdress: data.email
+            }).select('Username')
+            .then(function (answer) {
+                if (answer[0] === undefined) {
+                    insertValues(data,res);
+                } else {
+                    index.renderRegisterWithRenderHome({
+                        message: 'This email was already used',
+                        register: true
+                    }, res)
                 }
             })
-        );
+    } else {
+        index.renderRegisterWithRenderHome({
+            message: "Passwords don't match",
+            register: true,
+            username: data.username
+        }, res)
+    }
 });
+
 
 module.exports = router;
