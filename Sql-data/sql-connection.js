16686d2de019c2d32720fb83d825fba9b2808583
svend <sven.depickere@student.howest.be>
2018-05-26 16:49:28 +0200

    Sql connection

---
+++ Sql-data/sql-connection.js
@@ -0,0 +1,24 @@
+/**
+ * Created by svend on 18/02/2018.
+ */
+
+module.exports = ({
+    client: 'mysql',
+    connection: {
+        host: "localhost",
+        port: 3306,
+        database: "login",
+        user: "root",
+        password: ""
+    }
+});
+
+// const connection = mySql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     database: "login",
+//     user: "root",
+//     password: ""
+// });
+// connection.connect();
+
