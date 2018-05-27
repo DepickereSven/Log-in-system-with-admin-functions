66b8013bd1bd5273e5479d46732fd6196fbebfbd
svend <sven.depickere@student.howest.be>
2018-05-27 11:27:42 +0200

    Updated error's

--- routes/redirect/error.js
+++ routes/redirect/error.js
@@ -2,14 +2,20 @@
  * Created by svend on 27/05/2018.
  */
 
+const fileRead = require('../fileread');
+
 module.exports = (function () {
 
     let toSomeResources = function (res, data) {
+        fileRead.saveTofile({
+            username: data.username ? data.username : "not logged in user ",
+            message: data.message
+        });
         res.render('error', {
             message: data.message,
             username: data.username,
             error: {
-                status: 403
+                status: data.status
             }
         });
     };
