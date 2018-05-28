0119a3c243a972326858927ddedf3ced2a05867b
svend <sven.depickere@student.howest.be>
2018-05-28 17:24:27 +0200

    refactor userlist

--- routes/userList.js
+++ routes/userList.js
@@ -2,36 +2,40 @@
  Created by svend on 27/05/2018.
  **/
 
+const sockets = require('./sockets');
+const userDataList = require('./userDataList');
+
 module.exports = (function () {
 
-    const users = [];
 
     let addUser = function (username) {
         if (controlIfUserIsAlreadyActive(username)) {
             return false;
         } else {
-            users.push({
+            let newUser = {
                 user: username,
                 activeSince: new Date().toISOString().replace('T', ' ').replace('Z', '')
-            });
-            console.log(users);
+            };
+            userDataList.userData.push(newUser);
+            let index = userDataList.userData.findIndex((el => el.user === username));
+            sockets.showUsers(newUser, index);
             return true
         }
     };
 
     let removeUser = function (username) {
-        users.splice(users.findIndex((el => el.user === username)), 1);
-        console.log(users);
+        let index = userDataList.userData.findIndex((el => el.user === username));
+        userDataList.userData.splice(index, 1);
+        sockets.removeUser(userDataList.userData)
     };
 
     let controlIfUserIsAlreadyActive = function (username) {
-        return !!users.find((el => el.user === username));
+        return !!userDataList.userData.find((el => el.user === username));
     };
 
     return {
         addUser: addUser,
-        removeUser: removeUser,
-        usernames: users
+        removeUser: removeUser
     }
 
 })();
